6c2044901f966b8d7bd11c24b649c86b0a3dd9fc
Manjot Singh <sin13014.sheridancollege.ca>
2023-07-16 21:31:54 -0400

    added SpringSecurity and thymeleafSecurity dependency

--- src/main/java/ca/sheridancollege/sin13014/config/SecurityConfig.java
+++ src/main/java/ca/sheridancollege/sin13014/config/SecurityConfig.java
@@ -1,4 +1,26 @@
 package ca.sheridancollege.sin13014.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder){
+        UserDetails user1 = User.withUsername("user").password(passwordEncoder.encode("123")).roles("USER").build();
+        UserDetails user2 = User.withUsername("bartender").password(passwordEncoder.encode("123")).roles("BARTENDER").build();
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return passwordEncoder;
+    }
 }
